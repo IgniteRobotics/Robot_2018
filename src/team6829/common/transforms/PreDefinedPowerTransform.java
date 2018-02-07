4be84a7355c38ae758480555dcb7a236ac85a025
shintyl <14168765+shintyl@users.noreply.github.com>
2018-02-06 19:56:20 -0500

    Made better ecoder readings. also new transform

---
+++ src/team6829/common/transforms/PreDefinedPowerTransform.java
@@ -0,0 +1,11 @@
+package team6829.common.transforms;
+
+public class PreDefinedPowerTransform implements ITransform {
+
+	@Override
+	public double transform(double input) {
+		// TODO Auto-generated method stub
+		return 0.1;
+	}
+
+}
