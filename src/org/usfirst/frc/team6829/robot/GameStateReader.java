d287e3ced4eaaa41f1a024b98b95f45ff73081b5
techcrab101 <techcrab101@gmail.com>
2018-02-24 10:43:56 -0500

    code clean up

--- src/org/usfirst/frc/team6829/robot/GameStateReader.java
+++ src/org/usfirst/frc/team6829/robot/GameStateReader.java
@@ -7,23 +7,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class GameStateReader {
 
-	private boolean LeftPositionAuto;
-	private boolean MiddlePositionAuto;
-	private boolean RightPositionAuto;
+	private boolean LeftStartPositionAuto;
+	private boolean MiddleStartPositionAuto;
+	private boolean RightStartPositionAuto;
 
 	public void setRobotPosition() {
 
-		SmartDashboard.putBoolean("Left Position Auto", false);
-		SmartDashboard.putBoolean("Middle Position Auto", false);
-		SmartDashboard.putBoolean("Right Position Auto", false);
+		SmartDashboard.putBoolean("Left Start Position Auto", false);
+		SmartDashboard.putBoolean("Middle Start Position Auto", false);
+		SmartDashboard.putBoolean("Right Start Position Auto", false);
 
 	}
 
 	public Command gameStateReader(Map<String, Command> autonCommands) {
 
-		LeftPositionAuto = SmartDashboard.getBoolean("Left Position Auto", false);
-		MiddlePositionAuto = SmartDashboard.getBoolean("Middle Position Auto", false);
-		RightPositionAuto = SmartDashboard.getBoolean("Right Position Auto", false);
+		LeftStartPositionAuto = SmartDashboard.getBoolean("Left Start Position Auto", false);
+		MiddleStartPositionAuto = SmartDashboard.getBoolean("Middle Start Position Auto", false);
+		RightStartPositionAuto = SmartDashboard.getBoolean("Right Start Position Auto", false);
 
 		Command foo = null;
 
@@ -31,49 +31,65 @@ public class GameStateReader {
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
 		System.out.println(gameData);
 
-		if (LeftPositionAuto) {
+		if (LeftStartPositionAuto) {
 			System.out.println("Left Auto");  
 		}
-		if (MiddlePositionAuto) {
+		if (MiddleStartPositionAuto) {
 			System.out.println("Middle Auto");  
 		} 
-		if (RightPositionAuto) {
+		if (RightStartPositionAuto) {
 			System.out.println("Right Auto");  
 		}
 
 		if(gameData.charAt(0) == 'L') {
-			System.out.println("Left");
+			System.out.println("GameData: Left");
 		} else {
-			System.out.println("Right");
+			System.out.println("GameData: Right");
 		}
 
-		// Left Switch && Positioned Left 
-		if(gameData.charAt(0) == 'L' && LeftPositionAuto) {			
-
-			// TODO: Put left-switch auto code here
-
+		// Middle Start Position (generic drive forward) 
+		if(gameData.charAt(0) == 'L' && MiddleStartPositionAuto || gameData.charAt(0) == 'R' && MiddleStartPositionAuto) {			
 			foo = autonCommands.get("Go Straight");
-			System.out.println("Forwards (s-left)");
+			System.out.println("Forwards - Middle Start Position Auto");
 			return foo;
 		}
 
-		// Right Switch && Positioned Left
-		if (gameData.charAt(0) == 'R' && LeftPositionAuto) { 
-			// TODO: Put right-switch auto code here
-			System.out.println("Either Turn and go forwards or Loop around switch (s-right)");
+		// Right Switch Positions Auto
+		if (gameData.charAt(0) == 'R') { 
+			if(LeftStartPositionAuto) {
+				// TODO: Put right-switch/left position auto code here
+				System.out.println("Left start position (right switch)");
+			} 
+			
+//			if(MiddleStartPositionAuto) {
+//				// TODO: Put right-switch/middle position auto code here
+//				System.out.println("Middle start position (right switch)");
+//			}
+			
+			if(RightStartPositionAuto) {
+				// TODO: Put right-switch/right position auto code here
+				System.out.println("Right start position (right switch)");
+			}
 		}
 
-		// Left Switch && Positioned Right
-		if (gameData.charAt(0) == 'L' && RightPositionAuto) {
-			// TODO: Put right-switch auto code here
-			System.out.println("Either Turn and go forwards or Loop around switch (s-left)");
+		// Left Switch Positions Auto
+		if (gameData.charAt(0) == 'L') {
+			if(LeftStartPositionAuto) {
+				// TODO: Put left-switch/left position auto code here
+				System.out.println("Left start position (left switch)");
+			} 
+			
+//			if(MiddleStartPositionAuto) {
+//				// TODO: Put left-switch/middle position auto code here
+//				System.out.println("Middle start position (left switch)");
+//			}
+			
+			if(RightStartPositionAuto) {
+				// TODO: Put left-switch/right position auto code here
+				System.out.println("Right start position (left switch)");
+			}
 		}
 
-		// Right Switch && Positioned Right
-		if (gameData.charAt(0) == 'R' && RightPositionAuto) {
-			// TODO: Put right-switch auto code here
-			System.out.println("Forwards (s-right)");
-		}
 
 		return foo;
 	}
